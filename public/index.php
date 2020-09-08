3d3d0346996f20afc94c97267032c2f2b342a197
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-09-09 01:12:12 +0300

    Add search form with processing data to project

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория в которой будут храниться шаблоны
@@ -14,12 +16,26 @@ $container->set('renderer', function () {
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/users', function ($request, $response) {
+/*$app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
-});
+});*/
 
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
+});
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term', '');
+    if ($term !== '') {
+        $filteredUsers = array_filter($users, function($user) use ($term) {
+            return strpos($user, $term) !== false;
+        });
+        $usersForPage = array_values($filteredUsers);
+        $params = ['users' => $usersForPage, 'term' => $term];
+    } else {
+        $params = ['users' => $users, 'term' => ''];
+    }
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
 $app->get('/users/{id}', function ($request, $response, $args) {
