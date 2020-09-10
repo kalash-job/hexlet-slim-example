9409da4839671936ec0c9cc61f2ef8810811c16d
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-09-11 02:10:14 +0300

    Add names for routes and using of this names in templates and routers

--- public/index.php
+++ public/index.php
@@ -14,43 +14,41 @@ $container->set('renderer', function () {
 });
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+$router = $app->getRouteCollector()->getRouteParser();
 
-/*$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});*/
-
-/*$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});*/
-
-$app->get('/users', function ($request, $response) {
+$app->get('/users', function ($request, $response) use ($router) {
     $term = $request->getQueryParam('term', '');
     $users = json_decode(file_get_contents(__DIR__ . '/../data/users.json'), TRUE);
+    foreach ($users as $user) {
+        $route = $router->urlFor('user', ['id' => $user['id']]);
+        $routes[$user['id']] = $route;
+    }
     if ($term !== '') {
         $filteredUsers = array_filter($users, function($user) use ($term) {
             return strpos($user['nickname'], $term) !== false;
         });
         $usersForPage = array_values($filteredUsers);
-        $params = ['users' => $usersForPage, 'term' => $term];
+        $params = ['users' => $usersForPage, 'term' => $term, 'routes' => $routes];
     } else {
-        $params = ['users' => $users, 'term' => ''];
+        $params = ['users' => $users, 'term' => '', 'routes' => $routes];
     }
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users');
 
-$app->get('/users/new', function ($request, $response) use (&$lastId) {
+$app->get('/users/new', function ($request, $response) use ($router) {
     $lastId = json_decode(file_get_contents(__DIR__ . '/../data/id.json'), TRUE);
     $lastId ++;
     $id = $lastId;
     file_put_contents(__DIR__ . '/../data/id.json', json_encode($id));
     $params = [
         'user' => ['nickname' => '', 'email' => '', 'id' => $id],
-        'errors' => []
+        'errors' => [],
+        'route' => $router->urlFor('users')
     ];
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
+})->setName('new-user-form');
 
-$app->post('/users', function ($request, $response) {
+$app->post('/users', function ($request, $response) use ($router) {
     $user = $request->getParsedBodyParam('user');
     $validator = new Validator();
     $errors = $validator->validate($user);
@@ -58,18 +56,21 @@ $app->post('/users', function ($request, $response) {
         $users = json_decode(file_get_contents(__DIR__ . '/../data/users.json'), TRUE);
         $users[] = $user;
         file_put_contents(__DIR__ . '/../data/users.json', json_encode($users));
-        return $response->withRedirect('/users', 302);
+        $route = $router->urlFor('users');
+        return $response->withRedirect($route, 302);
     }
     $params = [
         'user' => $user,
         'errors' => $errors
     ];
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
+})->setName('save-user');
 
 $app->get('/users/{id}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('user');
+
+
 
 $app->run();
